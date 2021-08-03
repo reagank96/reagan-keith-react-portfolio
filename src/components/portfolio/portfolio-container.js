b8fe87f0337f1c0440655880a398088079d7d7d6
ReaganKeith <reagankeith25@gmail.com>
2021-08-02 21:32:19 -0600

    added portfolio items

---
+++ src/components/portfolio/portfolio-container.js
@@ -0,0 +1,35 @@
+import React, {Component} from "react";
+
+import PortfolioItem from "./portfolio-item";
+
+export default class PortfolioContainer extends Component {
+    constructor() {
+        super();
+
+       this.state = {
+           pageTitle: "Welcome to my portfolio",
+           data: [
+               {title: "Quip"},
+               {title: "Eventbrite"},
+               {title: "Ministry Safe"},
+               {title: "SwingAway"}
+           ]
+       };
+    }
+
+    portfolioItems() {
+        return this.state.data.map(item => {
+            return <PortfolioItem title={item.title} />;
+        })
+    }
+
+    render() {
+        return (
+            <div>
+                <h2>{this.state.pageTitle}</h2>
+
+                {this.portfolioItems()}
+            </div>
+        )
+    }
+}
