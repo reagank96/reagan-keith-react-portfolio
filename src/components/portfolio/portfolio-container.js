424ef3469d1c1df52b71d72d94d1c8a619f91457
ReaganKeith <reagankeith25@gmail.com>
2021-08-05 21:49:59 -0600

    portfolio-container-update

--- src/components/portfolio/portfolio-container.js
+++ src/components/portfolio/portfolio-container.js
@@ -1,35 +1,60 @@
-import React, {Component} from "react";
+import React, { Component } from "react";
 
 import PortfolioItem from "./portfolio-item";
 
 export default class PortfolioContainer extends Component {
-    constructor() {
-        super();
+  constructor() {
+    super();
 
-       this.state = {
-           pageTitle: "Welcome to my portfolio",
-           data: [
-               {title: "Quip"},
-               {title: "Eventbrite"},
-               {title: "Ministry Safe"},
-               {title: "SwingAway"}
-           ]
-       };
+    this.state = {
+      pageTitle: "Welcome to my portfolio",
+      isLoading: false,
+      data: [
+        { title: "Quip", category: "eCommerce" },
+        { title: "Eventbrite", category: "Scheduling" },
+        { title: "Ministry Safe", category: "Enterprise" },
+        { title: "SwingAway", category: "eCommerce" }
+      ]
+    };
+
+    this.handleFilter = this.handleFilter.bind(this);
+  }
+
+  handleFilter(filter) {
+    this.setState({
+      data: this.state.data.filter(item => {
+        return item.category === filter;
+      })
+    });
+  }
+
+  portfolioItems() {
+    return this.state.data.map(item => {
+      return <PortfolioItem title={item.title} url={"google.com"} />;
+    });
+  }
+
+  render() {
+    if (this.state.isLoading) {
+      return <div>Loading...</div>;
     }
 
-    portfolioItems() {
-        return this.state.data.map(item => {
-            return <PortfolioItem title={item.title} />;
-        })
-    }
+    return (
+      <div>
+        <h2>{this.state.pageTitle}</h2>
 
-    render() {
-        return (
-            <div>
-                <h2>{this.state.pageTitle}</h2>
+        <button onClick={() => this.handleFilter("eCommerce")}>
+          eCommerce
+        </button>
+        <button onClick={() => this.handleFilter("Scheduling")}>
+          Scheduling
+        </button>
+        <button onClick={() => this.handleFilter("Enterprise")}>
+          Enterprise
+        </button>
 
-                {this.portfolioItems()}
-            </div>
-        )
-    }
+        {this.portfolioItems()}
+      </div>
+    );
+  }
 }
--- src/components/portfolio/portfolio-item.js
+++ src/components/portfolio/portfolio-item.js
@@ -4,6 +4,7 @@ export default function(props) {
     return (
         <div>
             <h3>{props.title}</h3>
+            <h4>{props.url}</h4>
         </div>
     );
 }
