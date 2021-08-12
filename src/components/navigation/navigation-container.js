1c97695c9822dad1591d8359c7da20067379779b
ReaganKeith <reagankeith25@gmail.com>
2021-08-11 22:28:45 -0600

    started creating navbar

---
+++ src/components/navigation/navigation-container.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import { NavLink } from 'react-router-dom';
+
+export default class NavigationComponent extends Component {
+    constructor() {
+        super();
+    }
+
+    render() {
+        return (
+            <div>
+            <NavLink exact to="/">Home</NavLink>
+
+            <NavLink to="/about-me">About</NavLink>
+
+            <button>Contact</button>
+            <button>Blog</button>
+            {true ? <button>Add Blog</button> : null }
+            </div>
+        );
+    }
+}
