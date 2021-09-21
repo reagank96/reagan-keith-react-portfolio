0b7154c76f0f17e520fe8dd90833427a962bdc4d
ReaganKeith <reagankeith25@gmail.com>
2021-09-20 21:55:41 -0600

    portfolio form

--- src/components/pages/portfolio-manager.js
+++ src/components/pages/portfolio-manager.js
@@ -1,15 +1,63 @@
 import React, { Component } from "react";
+import axios from "axios";
+
+import PortfolioSidebarList from "../portfolio/portfolio-sidebar-list";
+import PortfolioForm from "../portfolio/portfolio-form";
 
 export default class PortfolioManager extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      portfolioItems: []
+    };
+
+    this.handleSuccessfulFormSubmission = this.handleSuccessfulFormSubmission.bind(this);
+    this.handleFormSubmuissionError = this.handleFormSubmuissionError.bind(this);
+  }
+
+  handleSuccessfulFormSubmission(portfolioItem) {
+    // TODO
+    // update the portfolioItems state
+    // and add the portfolioItem to the list
+  }
+
+
+  handleFormSubmuissionError(error) {
+    console.log("handleFormSubmissionError error", error);
+  }
+
+  getPortfolioItems() {
+    axios
+      .get("https://reagankeith.devcamp.space/portfolio/portfolio_items", {
+        withCredentials: true
+      })
+      .then(response => {
+        this.setState({
+          portfolioItems: [...response.data.portfolio_items]
+        });
+      })
+      .catch(error => {
+        console.log("error in getPortfolioItems", error);
+      });
+  }
+
+  componentDidMount() {
+    this.getPortfolioItems();
+  }
+
   render() {
     return (
-        <div className="portfolio-manager-wrapper">
+      <div className="portfolio-manager-wrapper">
         <div className="left-column">
-          <h1>Portfolio form....</h1>
+          <PortfolioForm
+            handleSuccessfulFormSubmission={this.handleSuccessfulFormSubmission}
+            handleFormSubmuissionError={this.handleFormSubmuissionError}
+          />
         </div>
-      
+
         <div className="right-column">
-          <h1>Portfolio sidebar....</h1>
+          <PortfolioSidebarList data={this.state.portfolioItems}/>
         </div>
       </div>
     );
