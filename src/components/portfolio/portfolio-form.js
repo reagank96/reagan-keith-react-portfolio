7a143d3f071b6538ff312e6715885c04bbf6bae8
ReaganKeith <reagankeith25@gmail.com>
2021-09-26 22:12:00 -0600

    finish portfolio form with delete option and added icons using font awesome library

--- src/components/portfolio/portfolio-form.js
+++ src/components/portfolio/portfolio-form.js
@@ -131,7 +131,7 @@ export default class PortfolioForm extends Component {
   render() {
     return (
         <form onSubmit={this.handleSubmit} className="portfolio-form-wrapper">
-          <div>
+          <div className="two-column">
             <input
               type="text"
               name="name"
@@ -149,7 +149,7 @@ export default class PortfolioForm extends Component {
             />
           </div>
 
-          <div>
+          <div className="two-column">
             <input
               type="text"
               name="position"
@@ -170,7 +170,7 @@ export default class PortfolioForm extends Component {
             </select>
           </div>
 
-          <div>
+          <div className="one-column">
             <textarea
               type="text"
               name="description"
@@ -186,25 +186,31 @@ export default class PortfolioForm extends Component {
               config={this.componentConfig()}
               djsConfig={this.djsConfig()}
               eventHandlers={this.handleThumbDrop()}
-            />
+            >
+                <div className="dz-message">Thumbnail</div>
+            </DropzoneComponent>
 
             <DropzoneComponent
               ref={this.bannerRef}
               config={this.componentConfig()}
               djsConfig={this.djsConfig()}
               eventHandlers={this.handleBannerDrop()}
-            />
+            >
+                <div className="dz-message">Banner</div>
+            </DropzoneComponent>
 
             <DropzoneComponent
               ref={this.logoRef}
               config={this.componentConfig()}
               djsConfig={this.djsConfig()}
               eventHandlers={this.handleLogoDrop()}
-            />
+            >
+                <div className="dz-message">Logo</div>
+            </DropzoneComponent>
           </div>
 
           <div>
-            <button type="submit">Save</button>
+            <button className="btn" type="submit">Save</button>
           </div>
         </form>
     );
